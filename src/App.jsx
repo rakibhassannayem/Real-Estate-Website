54b0c99185c804e9cb95eabfd9f0dd1f5813716a
rakibhassannayem <rakibhassannayem@gmail.com>
2025-02-22 19:14:13 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import About from './components/About'
+import Header from './components/Header'
+import Projects from './components/Projects'
+
+const App = () => {
+  return (
+    <div className='w-full overflow-hidden'>
+      
+      <Header/>
+      <About/>
+      <Projects/>
+    </div>
+  )
+}
+
+export default App
